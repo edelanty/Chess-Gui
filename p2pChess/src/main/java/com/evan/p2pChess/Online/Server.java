adc1565a0b772489dbb41f0b533b19bcbb193825
Evan Delanty <efdelanty@gmail.com>
2025-04-01 14:58:15 -0700

    Added some debugging info for online mode.

--- p2pChess/src/main/java/com/evan/p2pChess/Online/Server.java
+++ p2pChess/src/main/java/com/evan/p2pChess/Online/Server.java
@@ -2,6 +2,7 @@ package com.evan.p2pChess.Online;
 
 import java.io.*;
 import java.net.*;
+import java.util.Enumeration;
 
 public class Server implements NetworkConnection {
     private ServerSocket serverSocket;
@@ -11,6 +12,31 @@ public class Server implements NetworkConnection {
 
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
+        displayServerIP();
+    }
+
+    private void displayServerIP() {
+        try {
+            System.out.println("Server started. Your IP addresses:");
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                if (networkInterface.isUp() && !networkInterface.isLoopback()) {
+                    Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                    while (addresses.hasMoreElements()) {
+                        InetAddress address = addresses.nextElement();
+                        if (address instanceof Inet4Address) {
+                            System.out.println(networkInterface.getDisplayName() + ": " + address.getHostAddress());
+                        }
+                    }
+                }
+            }
+            System.out.println("Port: " + serverSocket.getLocalPort());
+            System.out.println("Share this information with your friend to connect!");
+        } catch (SocketException e) {
+            System.err.println("Error retrieving network interfaces: " + e.getMessage());
+        }
     }
 
     public void waitForConnection() throws IOException {
@@ -18,7 +44,7 @@ public class Server implements NetworkConnection {
         clientSocket = serverSocket.accept();
         input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         output = new PrintWriter(clientSocket.getOutputStream(), true);
-        System.out.println("Client connected.");
+        System.out.println("Client connected from: " + clientSocket.getInetAddress().getHostAddress());
     }
 
     public void sendMove(String move) throws IOException {
